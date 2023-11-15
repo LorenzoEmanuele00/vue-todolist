b471ac993e048a19f7903aa26f60a47da1736089
LorenzoEmanuele00 <lorenzoema2800@gmail.com>
2023-11-15 23:22:17 +0100

    add bonus 3 with text deco when done bool is true

--- js/script.js
+++ js/script.js
@@ -2,21 +2,27 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
-            toDoS: [
-                "Pulire camera",
-                "Buttare la spazzatura",
-                "Fare gli esercizi"
-            ],
-            newToDo: "",
+            toDoS: [],
+            newToDo: {
+                text: "",
+                done: false
+            }
         }
     },
     methods: {
         addToDo: function() {
-            this.toDoS.push(this.newToDo);
-            this.newToDo = "";
+            this.toDoS.push({...this.newToDo});
+            this.newToDo.text = "";
         },
         removeToDo: function(ind) {
             this.toDoS.splice(ind, 1);
         },
+        toDoDone: function(ind) {
+            if (this.toDoS[ind].done === false) {
+                this.toDoS[ind].done = true;
+            } else {
+                this.toDoS[ind].done = false;
+            }
+        }
     }
 }).mount("#app");
